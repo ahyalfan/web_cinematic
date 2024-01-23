a57bcb9f971843c9b3a6ebf456b995f8601ed2e7
ahyalfan <aslibojonegoro.1@gmail.com>
2024-01-23 16:26:14 +0700

    responsive success

--- resources/views/header/navbar.blade.php
+++ resources/views/header/navbar.blade.php
@@ -1,19 +1,19 @@
 <nav class="w-full h-[5.3rem] bg-slate-50 dark:bg-black drop-shadow-lg dark:shadow-slate-300 dark:text-white">
     <ul class="h-full flex flex-row items-center">
-       <li class="basis-1/4 pl-7">
-          <a href="/movie" class="uppercase font-inter font-medium mx-5 hover:text-ahy-400 dark:hover:text-ahy-700 transition duration-200">
-             Movie
+       <li class="basis-1/4 pl-2 md:pl-7">
+          <a href="/movie" class="uppercase font-inter text-small md:text-lg md:font-medium md:mx-5 hover:text-ahy-400 dark:hover:text-ahy-700 transition duration-200">
+            Tv Show
           </a>
-          <a href="/tvShow" class="uppercase font-inter font-medium mx-5 hover:text-ahy-400 dark:hover:text-ahy-700 transition duration-200">
-             Tv Show
+          <a href="/tvShow" class="uppercase font-inter text-small md:text-lg md:font-medium md:mx-5 hover:text-ahy-400 dark:hover:text-ahy-700 transition duration-200">
+            Movie
           </a>
        </li>
        <li class="basis-1/2 text-center">
-          <a href="/" class="uppercase font-quicksand font-semibold text-5xl hover:text-ahy-400 dark:hover:text-ahy-700 transition duration-200">
+          <a href="/" class="uppercase font-quicksand font-semibold text-2xl md:text-5xl hover:text-ahy-400 dark:hover:text-ahy-700 transition duration-200">
              Cinemation
           </a>
        </li>
-       <li class="basis-1/4 flex justify-end pr-7">
+       <li class="basis-1/4 flex justify-end pr-3 md:pr-7">
           <a href="">
              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:stroke-ahy-400 dark:hover:stroke-ahy-700 dark:stroke-white transition duration-200">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
